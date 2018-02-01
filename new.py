f0ae3daae7c12c72580e361ba7f883825ff6e65c
ggzor <30713864+ggzor@users.noreply.github.com>
2018-02-01 12:21:37 -0600

    Adding "new" implementation

---
+++ new.py
@@ -0,0 +1,18 @@
+from sys import argv
+from os.path import expandvars, join
+from os import getcwd
+from shutil import copy
+
+def make_new(filename):
+    template = expandvars("$GTEST_HOME/ctemplate.c")
+
+    if not filename.endswith(".c"):
+        filename += ".c"
+
+    copy(template, filename)
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        make_new(join(getcwd(), argv[1]))
+    else:
+        print("Usage: n <file>")
